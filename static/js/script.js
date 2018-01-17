a62a6d10446c68925070d4bf3a7d0fb9db9a96fb
Prasanna Inamdar <prasanna.inamdar77@gmail.com>
2018-01-17 17:23:43 -0600

    added vdo

--- static/js/script.js
+++ static/js/script.js
@@ -66,7 +66,7 @@ $(window).load(function () {
 
 $(".main_testimonial").owlCarousel({
 autoHeight:true,
-autoPlay: 5000, //Set AutoPlay to 3 seconds*/
+autoPlay: false, //Set AutoPlay to 3 seconds*/
 items : 1,
 itemsDesktop : [1199,3],
 itemsDesktopSmall : [979,3],
